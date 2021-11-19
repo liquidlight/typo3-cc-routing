ea7ab07d6c39242bcc0ebbe738a980c5c38c0866
Mike Street <mike@liquidlight.co.uk>
2021-11-19 17:16:19 +0000

    Chore: Lint PHP

--- ext_tables.php
+++ ext_tables.php
@@ -4,7 +4,7 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
 	function () {
-    	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_ccrouting_pathsegment', 'EXT:cc_routing/Resources/Private/Language/locallang_csh_tx_ccrouting_pathsegment.xlf');
-    	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ccrouting_pathsegment');
-    }
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_ccrouting_pathsegment', 'EXT:cc_routing/Resources/Private/Language/locallang_csh_tx_ccrouting_pathsegment.xlf');
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ccrouting_pathsegment');
+	}
 );
