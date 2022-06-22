13d0be99bde271eababa2a57c07f47570d6ebbd6
Mike Street <mike@liquidlight.co.uk>
2022-06-22 14:30:13 +0100

    Chore: Lint PHP

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    \CoelnConcept\CcRouting\Domain\Model\Pathsegment::class => [
-        'tableName' => 'tx_ccrouting_pathsegment',
-    ],
+	\CoelnConcept\CcRouting\Domain\Model\Pathsegment::class => [
+		'tableName' => 'tx_ccrouting_pathsegment',
+	],
  ];
