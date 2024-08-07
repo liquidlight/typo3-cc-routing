eefb65d5158d9e4fd8797b8b47fa39375211b5c6
Mike Street <mikestreety@gmail.com>
2024-08-07 20:45:56 +0100

    build(ext_emconf): Re-instate ex_emconf for TER

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'CC Routing',
+	'description' => 'Extended routing with database persistence',
+	'category' => 'fe',
+	'author' => 'Liquid Light ltd.',
+	'author_email' => 'info@liquidlight.co.uk',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '2.0.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '11.5.19-12.9.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
+];
